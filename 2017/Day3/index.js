e2e394eab1c2d2565acc175b4be2de27bf4a393d
sparcut <sparcut@users.noreply.github.com>
2017-12-07 15:02:09 +1100

    added Day 3 (2017) - incomplete

---
+++ 2017/Day3/index.js
@@ -0,0 +1,49 @@
+const PUZZLE_INPUT = '361527';
+
+// Part 1
+const SpiralStepCount = rawInput => {
+  // This makes my head hurt.
+
+
+  // const currentCords = [ 0, 0 ];
+  // const outerSize = [ 3, 3 ]; // Starting size 3x3
+
+  // const moveLeft = () => currentCords[0] += 1;
+  // const moveRight = () => currentCords[0] -= 1;
+  // const moveUp = () => currentCords[1] -= 1;
+  // const moveDown = () => currentCords[1] += 1;   
+
+
+
+  // let currentVal = 1;
+  // let startOfSquare = true;
+  // while(currentVal !== 9) {
+    
+  //   if(startOfSquare) {
+  //     startOfSquare = false;
+  //     moveLeft();
+  //   } else if(currentCords[0] === outerSize[0] && currentCords[1] > outer) {
+
+  //   }
+
+
+    
+  //   if(currentCords[0] === outerSize[0]) { // Right side
+
+  //   } else if(currentCords[0] === (outerSize[0])){}
+
+
+
+  //   if(currentCords === outerSize) {
+  //     outerSize = [ outerSize[0]+2, outerSize[1]+2 ]; // Expand size
+  //     currentCords = [ outerSize[0], outerSize[1] ];
+  //   } else {
+
+  //   }
+
+  //   currentVal += 1;
+  //   if(currentVal === (outerSize[0] * outerSize[1])) {
+  //     startOfSquare = true;
+  //   }
+  // }
+}
